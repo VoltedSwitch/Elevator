0531f35a18b715df06d36b81480226da9df4e4e2
VoltedSwitch <wirelessvoltswitch@gmail.com>
2025-07-12 11:41:32 +0500

    Create elevator app

---
+++ static/script.js
@@ -0,0 +1,19 @@
+let floor = 0;
+
+function updateMessage() {
+  document.getElementById(
+    "message"
+  ).textContent = `🚪 You are on Floor ${floor}`;
+}
+
+function goUp() {
+  floor++;
+  updateMessage();
+}
+
+function goDown() {
+  if (floor > 0) {
+    floor--;
+  }
+  updateMessage();
+}
